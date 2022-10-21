fcaebd360e836e795649ed73125cf1940c5ddfde
wisdomhove <hovewisdom@gmail.com>
2022-10-21 07:56:56 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NumberOfWords{
+
+    class Program{
+
+        static void Main(string[] args){
+
+            int a = 0;
+            int count = 1;
+
+            Console.WriteLine("Enter a sentence: ");
+            string str = Console.ReadLine();
+
+
+           for(a = 0; a <= str.Length-1; a++){
+                if(str[a] == ' ' || str[a] == '\t' ){
+                    count += 1;
+                }
+            }
+
+            Console.WriteLine(count);
+
+
+        }
+        
+    }
+}
